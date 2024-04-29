bea755d7413b3e45df07a97c1693633e475f14a5
varun <varunpoloju966@gmail.com>
2024-04-29 17:07:26 +0530

    adding child comp(newFriend) and passing data to Parent comp(App)

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,62 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavorite ? "(Fav)" : "dummy" }}</h2>
+    <button @click="toggelFavorite">Toggle fav</button>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAddress }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    //defines which props component recieves
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  emits: ["toggle-favorite"], //defines which custom events components emit
+  data() {
+    return {
+      detailsAreVisible: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggelFavorite() {
+      this.$emit("toggle-favorite", this.id);
+    },
+  },
+};
+</script>
